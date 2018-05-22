2c83faafdbb105f69a3d82fae7cea9faa14e92d8
Aditya Mehra <Aix.m@outlook.com>
2018-05-22 21:40:47 +0930

    Update for Branch 18.02

--- __init__.py
+++ __init__.py
@@ -1,3 +1,6 @@
+"""
+WhatsNearby Mycroft Skill.
+"""
 import re
 import sys
 import subprocess
@@ -7,7 +10,7 @@ from wireless import Wireless
 from adapt.intent import IntentBuilder
 from os.path import join, dirname
 from string import Template
-from mycroft.skills.core import MycroftSkill
+from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util import read_stripped_lines
 from mycroft.util.log import getLogger
 from mycroft.messagebus.message import Message
@@ -18,21 +21,24 @@ LOGGER = getLogger(__name__)
 
 
 class WhatsNearbySkill(MycroftSkill):
+    """
+    WhatsNearby Skill Class.
+    """    
     def __init__(self):
+        """
+        Initialization.
+        """
         super(WhatsNearbySkill, self).__init__(name="WhatsNearbySkill")
         self.places_index = dirname(__file__) + '/places.json'
         self.app_id = self.settings['app_id']
         self.app_code = self.settings['app_code']
-
-
-    def initialize(self):
         self.load_data_files(dirname(__file__))
          
-        search_nearby_places_intent = IntentBuilder("NearbyPlaces").\
-            require("SearchPlacesKeyword").build()
-        self.register_intent(search_nearby_places_intent, self.handle_search_nearby_places_intent)
-
+    @intent_handler(IntentBuilder("NearbyPlaces").require("SearchPlacesKeyword").build())
     def handle_search_nearby_places_intent(self, message):
+        """
+        Handle Search Nearby Keyword
+        """
         utterance = message.data.get('utterance').lower()
         utterance = utterance.replace(message.data.get('SearchPlacesKeyword'), '')
         searchString = utterance.replace(" ", "")
@@ -43,16 +49,19 @@ class WhatsNearbySkill(MycroftSkill):
         getlat = getcords['location']['lat']
         getlong = getcords['location']['lng']
         cat = self.filterCat(searchString)
-	sendappid = str(self.app_id)
-	sendappcode = str(self.app_code)
+        sendappid = str(self.app_id)
+        sendappcode = str(self.app_code)
         data = "?at={0},{1}&cat={2}&app_id={3}&app_code={4}".format(getlat, getlong, cat, self.app_id, self.app_code)
         LOGGER.info(url+data)
         response = requests.request(method,url+data)
-        self.speak("Following information was found");
-        self.enclosure.ws.emit(Message("placesObject", {'desktop': {'data': response.text, 'locallat': getlat, 'locallong': getlong, 'appid': sendappid, 'appcode': sendappcode}}))
+        if cat is not False:
+            self.speak("Following information was found");
+            self.enclosure.ws.emit(Message("placesObject", {'desktop': {'data': response.text, 'locallat': getlat, 'locallong': getlong, 'appid': sendappid, 'appcode': sendappcode}}))
         
     def getLocation(self):
-            
+        """
+        Get location from Wlan and WifiAccessPoints for Mozilla Location Services
+        """    
         postdata = {}
         ifaces = []
         flbmode = {
@@ -80,8 +89,8 @@ class WhatsNearbySkill(MycroftSkill):
             if "Frequency" in line:
                 frequencyStr = line.split(':')[1].split(' GHz')[0]
                 frequency = int(float(frequencyStr)*1000)
-                channel = int(line.split('(Channel ')[1].replace(')',''))
-                iface['channel'] = channel
+                #channel = int(line.split('(Channel ')[1].replace(')',''))
+                #iface['channel'] = channel
                 iface['frequency'] = frequency
 
         postdata['wifiAccessPoints'].sort(
@@ -90,7 +99,6 @@ class WhatsNearbySkill(MycroftSkill):
         del postdata['wifiAccessPoints'][5:]
         
         url = 'https://location.services.mozilla.com/v1/geolocate?key=test'
-        print json.dumps(postdata, sort_keys=True, indent=4, separators=(',', ': '))
         r = requests.post(url, data=json.dumps(postdata))
         getlocresult = json.loads(r.text)
 
@@ -98,6 +106,9 @@ class WhatsNearbySkill(MycroftSkill):
    
    
     def filterCat(self, keywords):
+        """
+        Filter location type
+        """    
         keyword = keywords.lower()
         with open(self.places_index) as json_data:
             d = json.load(json_data)
@@ -126,13 +137,19 @@ class WhatsNearbySkill(MycroftSkill):
             elif keyword in d["transport"]:
                 return "transport"
             else:
-                self.speak("Could not find {0} nearby".format(keyword))
-                return keyword
+                self.speak("Could not find {0}".format(keyword))
+                return False
         
         
     def stop(self):
+        """
+        Mycroft Stop Function
+        """
         pass
 
 
 def create_skill():
+    """
+    Mycroft Create Skill Function
+    """
     return WhatsNearbySkill()
